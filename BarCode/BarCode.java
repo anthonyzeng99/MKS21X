75cae8401111e8e289cad46e9c859a755a1d9a24
anthonyzeng99 <anthonyzeng99@gmail.com>
2015-12-21 18:54:57 -0500

    added constructors

--- BarCode/BarCode.java
+++ BarCode/BarCode.java
@@ -4,4 +4,16 @@ public class BarCode {
     private String _zip;
     private String[] BARCODES;
 
+    BARCODES = ["||:::",":::||","::|:|","::||:",":|::|",":|:|:",":||::","|:::|","|::|:","|:|::"];
+    //             0       1       2       3       4       5       6       7       8       9 
+
+    public BarCode(String zip) {
+	_zip = zip;
+	_checkDigit = checkSum();
+    }
+
+    public BarCode(BarCode x) {
+	this(x._zip_);
+    }
+
 }
